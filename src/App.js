c915400e4f865cfb69f837a227fcb03d602c7af9
Oliver Starckjohann <oliverstarckjohann@gmail.com>
2020-11-03 09:05:53 +0100

    addes comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
 
 function App() {
+  //Setup of the State, where all Data is Stored
   const [user, setUser] = useState({
     firstname: "",
     lastname: "",
   });
 
+  //Things ToDo when button is clicked
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(user);
   };
 
+  //Things done on every input character in a form-input-field
   const handleChange = (event) => {
+    //Writes to state
     setUser({
+      //keeps the existing Data in state
       ...user,
+      //Looks in which inpustfield we are by name and give the value to the matching target
       [event.target.name]: event.target.value,
     });
     console.log(user.firstname);
