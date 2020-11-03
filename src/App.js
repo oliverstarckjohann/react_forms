7118384ef2ce2ad7f48afab6df44e75c9dd7c246
Oliver Starckjohann <oliverstarckjohann@gmail.com>
2020-11-03 08:53:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({
+    firstname: "",
+    lastname: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(user);
+  };
+
+  const handleChange = (event) => {
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value,
+    });
+    console.log(user.firstname);
+    console.log(user.lastname);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Frirst Name:
+          <input
+            type="text"
+            name="firstname"
+            value={user.firstname}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="lastname"
+            value={user.lastname}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit" value="Abschicken" />
+      </form>
     </div>
   );
 }
